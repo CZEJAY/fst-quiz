141b7be3ebe1704d72f564ed40c4583e69c76c56
Caleb <calebjimmy67@gmail.com>
2025-01-11 06:25:26 +0100

    Update application metadata and enhance authentication middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,46 @@
-export { auth as middleware } from "@/auth";
+import { auth as middleware } from "@/auth";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+
+// @ts-ignore
+export default middleware(async (req, ctx) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  if (isApiRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+    return Response.redirect(
+      new URL(`/auth/signin?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+    );
+  }
+
+  return null; // Changed from new Response(null, { status: 204 })
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
