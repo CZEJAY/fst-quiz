7e762afefb2eb17c6400eda05355599fc727d50f
Caleb <calebjimmy67@gmail.com>
2025-01-11 06:50:16 +0100

    Refactor DashboardShell component to simplify props and improve readability

--- components/user/dashboard-shell.tsx
+++ components/user/dashboard-shell.tsx
@@ -3,12 +3,6 @@ import { cn } from "@/lib/utils";
 import { DashboardSidebar } from "@/components/user/dashboard-sidebar";
 import { SidebarInset, SidebarProvider } from "../ui/sidebar";
 
-interface DashboardShellProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-export function DashboardShell({
-  children,
-  className,
-  ...props
-}: DashboardShellProps) {
+export function DashboardShell({ children }: { children: React.ReactNode }) {
   return <main className="w-full">{children}</main>;
 }
